3d735162ee8db60b8c82cb68e06b19c9e98f2253
linus-k519 <56581587+linus-k519@users.noreply.github.com>
2021-04-17 15:05:52 +0200

    make it better

---
+++ main.py
@@ -0,0 +1,67 @@
+import json
+import random
+from time import sleep
+import flask
+from flask import Flask, render_template
+from markupsafe import Markup
+import data
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/api/flight_map')
+def flight_map():
+    current_map = dict()
+    for drone_id, drone in data.drones.items():
+        current_map[drone_id] = {
+            'id': drone['id'],
+            'position': drone['position']
+            # Ignore all other values
+        }
+    return current_map
+
+
+def event():
+    pass
+    for i in range(10):
+        sleep(random.uniform(0.01, 2.0))
+        drone = data.update_random_drone()
+        yield f'data: {json.dumps(drone)}\n\n'.encode('utf-8')
+
+
+@app.route('/stream')
+def stream():
+    return flask.Response(event(), mimetype="text/event-stream")
+
+
+def print_dict(content, nested=2):
+    output = ''
+    for key, value in content.items():
+        key = key.replace('_', ' ').capitalize()
+        if isinstance(value, dict):
+            output += f'<h{nested}>{key}</h{nested}>'
+            output += print_dict(value)
+        else:
+            output += f'<p>{key}: {value}</p>'
+    return output
+
+
+@app.route('/api/info/<drone_id>')
+def api_drone_info(drone_id):
+    drone_id = int(drone_id)
+    if drone_id not in data.drones:
+        return f'Error: Could not find drone with id {drone_id}.'
+    return data.drones[drone_id]
+
+
+@app.route('/info/<drone_id>')
+def drone_info(drone_id):
+    return render_template('info.html', content=Markup(print_dict(api_drone_info(drone_id))))
+
+
+app.run()
---
+++ static/index.js
@@ -0,0 +1,50 @@
+function render_drone(drone_id, drone) {
+    const coordinate_scale = 9
+    let dom_drone = document.getElementById(`drone_${drone_id}`)
+    let drone_x = drone['position']['latitude'] * coordinate_scale
+    let drone_y = drone['position']['longitude'] * coordinate_scale
+
+    const drone_icon = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-drone" width="30" height="30" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+      <path d="M10 10h4v4h-4z" />
+      <line x1="10" y1="10" x2="6.5" y2="6.5" />
+      <path d="M9.96 6a3.5 3.5 0 1 0 -3.96 3.96" />
+      <path d="M14 10l3.5 -3.5" />
+      <path d="M18 9.96a3.5 3.5 0 1 0 -3.96 -3.96" />
+      <line x1="14" y1="14" x2="17.5" y2="17.5" />
+      <path d="M14.04 18a3.5 3.5 0 1 0 3.96 -3.96" />
+      <line x1="10" y1="14" x2="6.5" y2="17.5" />
+      <path d="M6 14.04a3.5 3.5 0 1 0 3.96 3.96" />
+    </svg>`
+
+    if (dom_drone == null) {
+        document.getElementById('content').innerHTML += `
+            <div id="drone_${drone_id}" style="position: absolute; top: ${drone_x}px; left: ${drone_y}px">
+                <a href="info/${drone_id}">${drone_icon}</a>
+            </div>`
+    } else {
+        dom_drone.style.top = `${drone_x}px`
+        dom_drone.style.left =`${drone_y}px`
+    }
+}
+
+function load_data() {
+    fetch('api/flight_map')
+        .then(response => response.json())
+        .then(data => {
+            for (const [drone_id, drone] of Object.entries(data)) {
+                render_drone(drone_id, drone)
+            }
+        })
+}
+
+function sse() {
+    console.log('eventsource')
+    let event_src = new EventSource('/stream');
+    event_src.onmessage = function (e) {
+        const drone = JSON.parse(e.data)
+        render_drone(drone['id'], drone)
+    }
+}
+load_data()
+sse()
